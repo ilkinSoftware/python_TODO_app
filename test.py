297b0e50bad89e241564a3bfad19247cd7e02e6a
unknown <ilkin.suleymanov@khazar.org>
2021-04-21 21:05:58 +0400

    todo

---
+++ test.py
@@ -0,0 +1,11 @@
+ 
+
+liste = [("a",5), ("b",10), ("c",2), ("d",1), ("G", 0)]
+print(sorted(liste, key= lambda x: x[1]))
+print(liste)
+
+""" liste = [("a",5), ("b",10), ("c",2), ("d",1), ("G", 0)]
+
+liste.sort(key= lambda x: x[1])
+print(liste)
+ """
